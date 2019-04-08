b9d4cc3c51b8a168fdf2b5e297b61e573d234b1d
Eduardo Ferreira <mulberry.beacon@gmail.com>
2019-04-08 20:56:05 +0100

    Added dummy data. Refactored the code to get rid of the enum. Still needs a few tidbits before it's ready.

--- spec-builder.py
+++ spec-builder.py
@@ -1,9 +1,45 @@
 # -*- coding: utf-8 -*-
 
+"""
+PC configuration generator.
+
+Author: Eduardo Ferreira
+"""
+
 import json
 import itertools
 
-from component_type import ComponentType
+
+_COMPONENT_TYPES = [
+    {
+        'name': 'Case',
+        'file': 'case.json'
+    },
+    {
+        'name': 'Graphics card',
+        'file': 'gpu.json',
+    },
+    {
+        'name': 'Memory',
+        'file': 'memory.json'
+    },
+    {
+        'name': 'Motherboard',
+        'file': 'motherboard.json'
+    },
+    {
+        'name': 'Power supply',
+        'file': 'psu.json'
+    },
+    {
+        'name': 'Processor',
+        'file': 'cpu.json'
+    },
+    {
+        'name': 'Storage',
+        'file': 'storage.json'
+    }
+]
 
 
 def load_file(filename: str) -> list:
@@ -22,26 +58,30 @@ def load_file(filename: str) -> list:
     except FileNotFoundError:
         print('File "{}" could not be found.'.format(filename))
 
-    return None
-
 
 def load_data() -> list:
     """
     Loads the data from the files and generates all possible combinations for the components.
+
+    :return:
+        The list of possible configurations
     """
     components = []
 
-    for ct in ComponentType:
-        component = dict(ct.value)
-        components.append(load_file(component['file']))
+    for ct in _COMPONENT_TYPES:
+        components.append(load_file(ct['file']))
 
     return list(itertools.product(*components))
 
 
-def transform_data():
-    #for subset in result:
-    #    print(json.dumps(subset, indent=4))
+def build():
+    result = [
+        {_COMPONENT_TYPES[idx]['name']: value for idx, value in enumerate(configuration)}
+        for configuration in load_data()
+    ]
+
+    return json.dumps(result, indent=4)
 
 
 if __name__ == '__main__':
-    transform_data()
+    print(build())
